4e168f7ea91762b77fa77a2a8f5da2aa88f44e21
David Belnap <david.belnap6@gmail.com>
2017-09-29 09:07:10 -0600

    rspec added

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :first_name, :last_name
+
+  def self.by_last_name(asc = true)
+  	asc ? order(:last_name) : order(last_name: :desc)
+  end
+
+  def info
+  	"#{email} has signed in: #{sign_in_count} times".
+  end
+
+  def full_name
+  	"#{last_name}, #{first_name}"
+  end
+
+  def display_name
+  	"#{first_name} #{last_name}"
+  end
+
+  def has_signed_in?
+  	sign_in_count > 0
+  end
 end
